f3d1525f5122b84eab0547e9a252fd733763f9cc
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-11-02 09:13:07 -0700

    Create empty Express Routers

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+// Body-parsing middleware
+app.use(express.urlendcoded({ extended: true }));
+app.use(express.json());
+
+// API routes
+app.use("/api", require("./api"));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status ?? 500;
+  const message = err.message ?? "Internal server error.";
+  res.status(status).json({ message });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
